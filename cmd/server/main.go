92586a2e60ec663faa250437443f3de8caf84afc
leptis <leptishome@gmail.com>
2019-09-18 20:34:12 +0000

    -

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"flag"
+	"github.com/BurntSushi/toml"
 	"log"
 	"nirs_web_app/internal/app/server"
 )
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "congif-path", "configs/apiserver.toml", "path to server config file")
+}
+
 func main(){
-	s := server.New()
+	flag.Parse()
+	config := server.NewConfig()
+
+	if _, err := toml.DecodeFile(configPath, config); err != nil {
+		log.Fatal(err)
+	}
+
+	s := server.New(config)
 	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
